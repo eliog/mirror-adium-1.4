6c5445f66142220e5efa96045286eea3d3714509
Evan Schoenberg <none@none>
2010-11-01 17:40:12 -0500

    Fixed a warning introduced in 8b2a6ff84514. Refs #12339

--- Source/AITemporaryIRCAccountWindowController.m
+++ Source/AITemporaryIRCAccountWindowController.m
@@ -14,6 +14,7 @@
 #import "AIServiceMenu.h"
 #import <AIUtilities/AIStringAdditions.h>
 #import <AIUtilities/AIStringFormatter.h>
+#import <Adium/AIChatControllerProtocol.h>
 
 @implementation AITemporaryIRCAccountWindowController
 
