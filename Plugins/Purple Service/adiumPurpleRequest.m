6011c80b511cafe0881113924bdd45e72633b8be
Evan Schoenberg <none@none>
2010-11-12 19:11:06 -0600

    adiumPurpleRequestActionWithIcon support (Only new UI callback in libpurple 2.7.0 per Changelog.API)

--- Plugins/Purple Service/adiumPurpleRequest.m
+++ Plugins/Purple Service/adiumPurpleRequest.m
@@ -142,12 +142,14 @@ static void *adiumPurpleRequestChoice(const char *title, const char *primary,
 	return nil;
 }
 
-//Purple requests the user take an action such as accept or deny a buddy's attempt to add us to her list 
-static void *adiumPurpleRequestAction(const char *title, const char *primary,
-									const char *secondary, int default_action,
-									PurpleAccount *account, const char *who, PurpleConversation *conv,
-									void *userData,
-									size_t actionCount, va_list actions)
+/** @see purple_request_action_with_icon_varg(). */
+static void *adiumPurpleRequestActionWithIcon(const char *title, const char *primary,
+								  const char *secondary, int default_action,
+								  PurpleAccount *account, const char *who,
+								  PurpleConversation *conv, 
+								  gconstpointer icon_data, gsize icon_size,
+								  void *user_data,
+								  size_t action_count, va_list actions);
 {
     NSString			*titleString = (title ? [NSString stringWithUTF8String:title] : @"");
 	NSString			*primaryString = (primary ? [NSString stringWithUTF8String:primary] : nil);
@@ -240,10 +242,38 @@ static void *adiumPurpleRequestAction(const char *title, const char *primary,
 			[infoDict setObject:[NSString stringWithUTF8String:who] forKey:@"who"];
 		}
 
+		if (icon_data && (icon_size > 0)) {
+			NSData *imageData = [NSData dataWithBytes:icon_data length:icon_size];
+			NSImage *image = [[[NSImage alloc] initWithData:imageData] autorelease];
+			if (image) 
+				[infoDict setObject:image forKey:@"Image"];
+		}
+
 		requestController = [ESPurpleRequestActionController showActionWindowWithDict:infoDict];
 	}
 
 	return requestController;
+}
+
+
+}
+
+//Purple requests the user take an action such as accept or deny a buddy's attempt to add us to her list 
+static void *adiumPurpleRequestAction(const char *title, const char *primary,
+									  const char *secondary, int default_action,
+									  PurpleAccount *account, const char *who,
+									  PurpleConversation *conv,
+									  void *userData,
+									  size_t actionCount, va_list actions)
+{
+	adiumPurpleRequestActionWithIcon(title, primary,
+									 secondary, default_action,
+									 account, who,
+									 conv,
+									 /* iconData */ NULL, /* iconSize */ 0,
+									 userData,
+									 actionCount, actions);
+								 
 }
 
 static void *adiumPurpleRequestFields(const char *title, const char *primary,
@@ -396,6 +426,7 @@ static PurpleRequestUiOps adiumPurpleRequestOps = {
 	adiumPurpleRequestFile,
     adiumPurpleRequestClose,
 	adiumPurpleRequestFolder,
+	adiumPurpleRequestActionWithIcon,
 	NULL, /* reserved */
 	NULL, /* reserved */
 	NULL, /* reserved */
