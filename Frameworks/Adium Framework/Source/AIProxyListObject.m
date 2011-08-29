ab0ba08a2d2d8ce067648b8ecaab711c7e247549
Evan Schoenberg <none@none>
2011-08-28 21:08:25 -0500

    Increased comment detail (transplanted from 03b0a921bb0fbc182577093fb9ec529af2d9b86d)
    --HG--
    extra : transplant_source : %03%B0%A9%21%BB%0F%BC%18%25w%09%3F%B9%ECR%9A%F2%D9%B8m

--- Frameworks/Adium Framework/Source/AIProxyListObject.m
+++ Frameworks/Adium Framework/Source/AIProxyListObject.m
@@ -48,7 +48,12 @@ static NSMutableDictionary *proxyDict;
 	proxy = [proxyDict objectForKey:key];
 
 	if (proxy && proxy.listObject != inListObject) {
-        /* This is a memory management failure; AIContactController stopped tracking a list object, but it never deallocated. -evands 8/7/11 */
+        /* This is generally a memory management failure; AIContactController stopped tracking a list object, but it never deallocated and
+		 * so never called [AIProxyListObject releaseProxyObject:].
+		 *
+		 * I've seen one case where proxy.listObject referred to a zombie object. I can't reproduce. If that happens, we'll get a crash here in
+		 * debug mode only.  -evands 8/28/11
+		 */
 		AILogWithSignature(@"%@ was leaked! Meh.", proxy.listObject);
 
 		proxy.listObject = inListObject;
@@ -61,7 +66,6 @@ static NSMutableDictionary *proxyDict;
 		proxy.containingObject = inContainingObject;
 		proxy.key = key;
 		[inListObject noteProxyObject:proxy];
-
 		[proxyDict setObject:proxy
 					  forKey:key];
 		[proxy release];
@@ -77,6 +81,7 @@ static NSMutableDictionary *proxyDict;
  */
 + (void)releaseProxyObject:(AIProxyListObject *)proxyObject
 {
+	AILogWithSignature(@"%@", proxyObject);
 	[[proxyObject retain] autorelease];
 	proxyObject.listObject = nil;
 	[proxyDict removeObjectForKey:proxyObject.key];
